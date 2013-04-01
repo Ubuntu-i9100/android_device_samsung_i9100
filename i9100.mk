e520ef520ed4bdc168fce88fade52410985af376
Max Wällstedt <max.wallstedt@gmail.com>
2013-04-01 22:33:06 +0200

    Grid unit update and enable hardware keys

--- i9100.mk
+++ i9100.mk
@@ -56,7 +56,7 @@ PRODUCT_COPY_FILES += \
 
 # Ubuntu-Session
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/ubuntu/ubuntu-session:system/ubuntu-session \
-    $(LOCAL_PATH)/ubuntu/99ubuntu-session:system/etc/init.d/99ubuntu-session
-
+    $(LOCAL_PATH)/ubuntu/GT-I9100.conf:system/GT-I9100.conf \
+    $(LOCAL_PATH)/ubuntu/99ubuntu-session:system/etc/init.d/99ubuntu-session \
+    $(LOCAL_PATH)/ubuntu/Shell.qml:system/Shell.qml
 $(call inherit-product-if-exists, vendor/samsung/i9100/i9100-vendor.mk)
